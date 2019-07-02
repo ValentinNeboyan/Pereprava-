f967fefaa52d112e355f06c51f688305a6413699
ValentinNeboyan <valentinneboyan@gmail.com>
2019-07-02 22:14:07 +0300

    hide add_to_cart hooks

--- wp-content/plugins/woocommerce/includes/wc-template-hooks.php
+++ wp-content/plugins/woocommerce/includes/wc-template-hooks.php
@@ -180,6 +180,15 @@ add_action( 'woocommerce_external_add_to_cart', 'woocommerce_external_add_to_car
 add_action( 'woocommerce_single_variation', 'woocommerce_single_variation', 10 );
 add_action( 'woocommerce_single_variation', 'woocommerce_single_variation_add_to_cart_button', 20 );
 
+
+remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_add_to_cart', 30 );
+remove_action( 'woocommerce_simple_add_to_cart', 'woocommerce_simple_add_to_cart', 30 );
+remove_action( 'woocommerce_grouped_add_to_cart', 'woocommerce_grouped_add_to_cart', 30 );
+remove_action( 'woocommerce_variable_add_to_cart', 'woocommerce_variable_add_to_cart', 30 );
+remove_action( 'woocommerce_external_add_to_cart', 'woocommerce_external_add_to_cart', 30 );
+remove_action( 'woocommerce_single_variation', 'woocommerce_single_variation', 10 );
+remove_action( 'woocommerce_single_variation', 'woocommerce_single_variation_add_to_cart_button', 20 );
+
 /**
  * Pagination after shop loops.
  *
