362920b8bff07d318aac01ebe4b7f95597d7dd46
ValentinNeboyan <valentinneboyan@gmail.com>
2019-07-06 23:31:35 +0300

    remove sidebar and widgets from single product page

--- wp-content/themes/storecommerce/functions.php
+++ wp-content/themes/storecommerce/functions.php
@@ -360,3 +360,11 @@ function print_pre($args)
     }
 
 }
+
+remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_add_to_cart', 30 );
+remove_action( 'woocommerce_simple_add_to_cart', 'woocommerce_simple_add_to_cart', 30 );
+remove_action( 'woocommerce_grouped_add_to_cart', 'woocommerce_grouped_add_to_cart', 30 );
+remove_action( 'woocommerce_variable_add_to_cart', 'woocommerce_variable_add_to_cart', 30 );
+remove_action( 'woocommerce_external_add_to_cart', 'woocommerce_external_add_to_cart', 30 );
+remove_action( 'woocommerce_single_variation', 'woocommerce_single_variation', 10 );
+remove_action( 'woocommerce_single_variation', 'woocommerce_single_variation_add_to_cart_button', 20 );
